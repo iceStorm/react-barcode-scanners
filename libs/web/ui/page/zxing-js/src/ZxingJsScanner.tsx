451c85c694a5353df41d98d32b3d2562ff4a12a5
Nguyen Anh Tuan <hauduedaiduong@gmail.com>
2023-03-19 13:34:09 +0700

    Style zxing-js elements

--- libs/web/ui/page/zxing-js/src/ZxingJsScanner.tsx
+++ libs/web/ui/page/zxing-js/src/ZxingJsScanner.tsx
@@ -2,11 +2,14 @@ import { useContext, useEffect, useRef, useState } from 'react'
 
 import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library'
 import { ContextBarcodeScanner } from '@react-barcode-scanners/web/data-access/context/barcode-scanner'
+import clsx from 'clsx'
 
 export function ZxingJsScanner() {
   const { onBarcodeDetected, onError } = useContext(ContextBarcodeScanner)
 
   const codeReader = useRef<BrowserMultiFormatReader>()
+  const [isScanning, setIsScanning] = useState(false)
+  const [isStartingScanning, setIsStartingScanning] = useState(false)
 
   const [selectedDeviceId, setSelectedDeviceId] = useState<string>('')
   const [deviceIdsList, setDeviceIdsList] = useState<MediaDeviceInfo[]>([])
@@ -39,6 +42,7 @@ export function ZxingJsScanner() {
 
   function resetReader() {
     codeReader.current?.reset()
+    setIsScanning(false)
   }
 
   function startScanning() {
@@ -46,49 +50,72 @@ export function ZxingJsScanner() {
       return
     }
 
-    codeReader.current.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-      if (result) {
-        console.log(result)
-        onBarcodeDetected(result.getText())
-      }
+    setIsStartingScanning(true)
 
-      if (err && !(err instanceof NotFoundException)) {
-        console.error(err)
-        onError(err)
-      }
-    })
+    codeReader.current
+      .decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+        if (result) {
+          console.log(result)
+          onBarcodeDetected(result.getText())
+        }
+
+        if (err && !(err instanceof NotFoundException)) {
+          console.error(err)
+          onError(err)
+        }
+      })
+      .then(() => {
+        setIsScanning(true)
+      })
+      .catch((error) => {
+        onError(error)
+        setIsScanning(false)
+      })
+      .finally(() => {
+        setIsStartingScanning(false)
+      })
 
     console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
   }
 
   return (
-    <div>
-      <div className="p-3 bg-stone-300">
-        <div className="text-center">
-          <label htmlFor="sourceSelect">Change video source:</label>
-          <select
-            id="sourceSelect"
-            style={{ maxWidth: '400px' }}
-            onChange={(id) => onDeviceIdChanged(id.target.value)}
-          >
-            {deviceIdsList.map((device) => {
-              return (
-                <option key={device.deviceId} value={device.deviceId}>
-                  {device.label}
-                </option>
-              )
-            })}
-          </select>
-        </div>
+    <div className="relative">
+      <div className={clsx('p-3 bg-stone-300 border-b flex flex-col gap-3', 'sticky top-0')}>
+        {!isScanning && !isStartingScanning && (
+          <div className="text-center">
+            <label htmlFor="sourceSelect">Change video source:</label>
+            <select
+              id="sourceSelect"
+              style={{ maxWidth: '400px' }}
+              onChange={(id) => onDeviceIdChanged(id.target.value)}
+            >
+              {deviceIdsList.map((device) => {
+                return (
+                  <option key={device.deviceId} value={device.deviceId}>
+                    {device.label}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+        )}
 
-        <div className="flex justify-center gap-5 mt-3">
-          <button className="button border" onClick={() => startScanning()}>
-            Start Scanning
-          </button>
+        <div className="flex justify-center gap-5">
+          {!isScanning && (
+            <button
+              className="button border"
+              onClick={() => startScanning()}
+              disabled={isStartingScanning}
+            >
+              {isStartingScanning ? 'Loading camera...' : 'Start Scanning'}
+            </button>
+          )}
 
-          <button className="button border" onClick={() => resetReader()}>
-            Reset
-          </button>
+          {isScanning && (
+            <button className="button border" onClick={() => resetReader()}>
+              Pause Scanning
+            </button>
+          )}
         </div>
       </div>
 
